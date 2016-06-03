6d9c0231ed2135cefd32b44bc86cb5c6ec9a829a
Henry <henry.li03@sap.com>
2016-06-03 16:41:42 +0800

    Bug fix.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const fs = require("fs");
 const path = require("path");
 const loaderUtils = require("loader-utils");
 
-module.exports = function(source)
+let loaderOptions = null;
+
+function ui5Loader(source)
 {
     this.cacheable();
+
+    loaderOptions = loaderUtils.parseQuery(this.query);
 
     const webpackRemainingChain = loaderUtils.getRemainingRequest(this).split('!');
     const filename = webpackRemainingChain[webpackRemainingChain.length - 1];
@@ -13,23 +17,15 @@ module.exports = function(source)
     if (!definePart.endsWith("[]"))
     {
         const group = definePart.match(/\[(.+)\]/)[1];
-        const loaderOptions = loaderUtils.parseQuery(this.query);
-        const sourceRoot = path.resolve(loaderOptions.sourceRoot ? loaderOptions.sourceRoot : process.cwd());
         let dependencies = group.split(", ").map(d => d.replace(/"/g, ""));
         const requires = [];
+
         dependencies = dependencies.map(d => {
-            let absPath = path.resolve(sourceRoot, "./" + d);
-            if (!absPath.endsWith(".js"))
+            const absPath = resolveModule(d, filename);
+            if (absPath !== null)
             {
-                absPath += ".js";
+                requires.push(`require("${absPath}");\n`);
             }
-            if (!fs.existsSync(absPath))
-            {
-                return null;
-            }
-            const relPath = path.relative(path.dirname(filename), absPath);
-            requires.push(`require("${relPath}")`);
-            return relPath;
         });
 
         source = requires.join(";\n") + "\n" + source;
@@ -37,3 +33,27 @@ module.exports = function(source)
 
     this.callback(null, source);
 };
+
+function resolveModule(modulePath, sourceFilename)
+{
+    const sourceRoot = path.resolve(loaderOptions.sourceRoot ? loaderOptions.sourceRoot : process.cwd());
+    let absPath = path.resolve(sourceRoot, "./" + modulePath);
+    if (!absPath.endsWith(".js"))
+    {
+        absPath += ".js";
+    }
+    if (fs.existsSync(absPath))
+    {
+        return absPath;
+    }
+    else
+    {
+        if (!modulePath.startsWith("sap/ui/"))
+        {
+            console.warn(`WARN: Dependency "${modulePath}" of "${path.relative(sourceRoot, sourceFilename)}" is not found in "${sourceRoot}".`);
+        }
+        return null;
+    }
+}
+
+module.exports = ui5Loader;
